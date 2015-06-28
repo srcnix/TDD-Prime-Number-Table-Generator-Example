e0802421d022474fa9f5549ffedffb46b14b455e
Steve Clarke <srcnix@gmail.com>
2015-06-28 13:04:17 +0100

    PrimeNumber now calculates multiplication array

--- lib/prime_number.rb
+++ lib/prime_number.rb
@@ -4,4 +4,20 @@ class PrimeNumberCalc
   def calculate_prime_numbers_from_max(max)
     Prime.take(max)
   end
+
+  def calculate_multiplication_table(prime_numbers)
+    table = {}
+
+    prime_numbers.each do |prime_number_to_multiply|
+      multiplications = []
+
+      prime_numbers.each do |prime_number|
+        multiplications << prime_number_to_multiply * prime_number
+      end
+
+      table[prime_number_to_multiply] = multiplications
+    end
+
+    table
+  end
 end
